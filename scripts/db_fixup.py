de1233a6e4123a876679c537fb2a95bc703282f3
Stu Perry <stewart.c.perry@gmail.com>
2014-03-16 21:18:06 +0000

    add juniors to db

--- scripts/db_fixup.py
+++ scripts/db_fixup.py
@@ -66,6 +66,19 @@ def updateUsers(users):
     if len(result) == 0:
       dbh.update("insert into User set Name = %s", [user])
       print user
+
+def getEmail(users):
+  dbh = DataBase()
+  for user in users:
+    result = dbh.queryAndStore("select Email from User where Name = %s", [user])
+    if len(result) == 0:
+      result = dbh.queryAndStore("select Email from BookingSystemContact where Name = %s", [user])
+      if len(result) == 0:
+        sys.stderr.write("Missing user: %(user)s\n" % locals())
+        continue
+      print result[0][0]
+    else:
+      print result[0][0]
   
 def updateSeeds(tournamentId, filename):
   fh = open(os.path.expanduser(filename))
@@ -81,4 +94,6 @@ if __name__ == "__main__":
 #  for i in range(1,9):
 #    addMatchNumbers(i)
 #  updateUsers(["Adrian Hayward", "Andrew Grace", "Andy Todd", "Anthony Muggeridge", "Brian Greatorex", "Chris Bartlett", "Craig Swinerd", "Danny Jones", "Dave Wallond", "David Alford", "David Dommett", "David Guy", "David Ironside", "Dennis White", "Diane Benford", "Ed Walters", "Elliot Lee", "Eric Butterworth", "Freddie Lawson", "Gerry Summers", "Graham Norton", "Hamza Ali", "Iain Bremner", "Ian Dermody", "Jack Ross", "Janik Karunaratne", "John Bryant", "John Hughes", "Karl Loynton", "Keith Brakefield", "Keith Holdaway", "Kerryn Bartlett", "Kieron Harwood", "Leigh Masters", "Leroy Valentine", "Martin Shelley", "Mike Herington", "Mike McElhatton", "Mike Rivers", "Mike Wardle", "Nicholas Hiley", "Paddy Bascombe", "Paul Garbutt", "Paul Perry", "Peter Richardson", "Ragoo Pema", "Ralph Goldstein", "Rich Jamieson", "Rob Heasman", "Robin Griffiths", "Rob Kemp", "Rob Ready", "Ryan McLaughlin", "Sharon Bains", "Simon Browne", "Spencer Harris", "Stewart Perry", "Sukhy Bains", "Toby Fenton", "Tom Pewter", "Usman Hussain"])
-  updateSeeds(sys.argv[1], sys.argv[2])
+#  updateSeeds(sys.argv[1], sys.argv[2])
+#  getEmail(["Paul Reilly", "Michael Wardle", "Paul Garbutt", "Peter Richardson", "Dave Wallond", "Adrian Hayward", "Rob Heasman", "Jack Ross", "Stuart Bicknell", "John Hughes", "Chris Burton", "Herman Van Den Berg", "Noel Laycock", "Rob Kemp", "Spencer Harris", "Ian Salmond", "Chris Bartlett", "Kieron Harwood", "Freddie Lawson", "Mike McElhatton", "Graham Norton", "Usman Hussain", "David Lyscom", "Patrick Bascombe", "Ivor Simpson", "Paul Perry", "Elliot Lee", "Keith Holdaway", "David Greatorex", "Martin Shelley", "Andy Bartlett", "Jez Foulsham", "Danny Jones", "Shahid Azeem", "John Bryant", "Leroy Valentine", "Mike Neale", "Peter Key", "Sarah Verdin", "Kerryn Bartlett", "Simon Browne", "David Webb", "Eric Butterworth", "Toby Fenton", "Ian Dermody", "David Dommett", "Mike Thacker", "Tito Nath", "Brian Greatorex", "David Guy", "Mike Calvert", "Phil Peakin", "Nicky Hayden", "Rob Ready", "Louise Bacon", "Annie Lane", "Diane Benford"])
+  updateUsers(["Andrea Mercer", "Alex Mercer", "Mark Tuddenham", "Ben Payne", "Oliver Eager", "Tvishi Agarwal", "Linus Fuge", "Alex McLean", "Toby Harris", "Amy Price", "Tiffany Kuk", "Dominic Kuk", "Benjamin Kuk", "Stephan Grace", "Philip Grace", "Nick Hiley", "James Bremner", "Hamza Ali", "Muneer Ali", "George Gervasio", "Jack Evans", "Sachi Raju", "Hugh Boorman", "Rajin Mangat"])
