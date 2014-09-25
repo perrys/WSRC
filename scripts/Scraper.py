cd9e7742b45a80951b49326847f6a6a21a4e363a
Stu Perry <stewart.c.perry@gmail.com>
2014-09-25 18:45:04 +0100

    Cosmetic changes, minor refactoring and documentation/comments

--- scripts/Scraper.py
+++ scripts/Scraper.py
@@ -9,26 +9,36 @@ from GBEireTimeZone import GBEireTimeZone
 
 UK_TZINFO = GBEireTimeZone()
 
-def tagGenerator(first, func=lambda(x): x.next_sibling, filt=lambda(x): hasattr(x, "name")):
-  while first is not None:
-    if filt(first):
-      yield first
-    first = func(first)
-
-def convertNBSP(s):
-  return s.replace(u'\xa0', ' ')
+def tagGenerator(head, next_func=lambda(x): x.next_sibling, filt=lambda(x): hasattr(x, "name")):
+  """Generator for tag collections. 
+  HEAD is the first element in the collection
+  NEXT_FUNC provides the next element given the current one
+  FILT is a filter function; return False to reject the current element and move to the next
+  """
+  while head is not None:
+    if filt(head):
+      yield head
+    head = next_func(head)
 
 def cellContent(c):
+  def convertNBSP(s):
+    return s.replace(u'\xa0', ' ')
   return convertNBSP(c.string)
 
-def parseBooking(cell):
+def processBooking(cell):
+  """Parse the cell contents into a either a string (in the case of
+  text-only content, such as dittos), or a partially constructed Event
+  object. The Event contains just the description (i.e. member name)
+  and link. The time fields are added later as part of row/page
+  processing"""
   link = cell.a
   if link is None:
     return cellContent(cell)
   return Event(cellContent(link), 'http://www.court-booking.co.uk/WokingSquashClub/' + link['href'])
 
-def parseWeekRow(row):
-
+def processWeekRow(row):
+  """Parse a 15-minute row in the weekly timetable, returning a time
+  string and a list of 7 Event objects, or None if the cell is empty"""
   class Result:
     def __init__(self, time, bookings):
       time = datetime.datetime.strptime(time, '%H:%M')
@@ -37,29 +47,37 @@ def parseWeekRow(row):
 
   slots = []
   first = row.td
+  result = Result(first.a.text, []) 
 
+  # filter function for the row generator, excludes the first and lass
+  # columns which have css class "red"
   def filtfunc(cell):
     if hasattr(cell, "name") and cell.name == 'td':
       cssclasses = cell.get("class") or []
       return "red" not in cssclasses
     return False
 
-  result = Result(first.a.text, []) 
-
+  # loop over valid cells in the row
   for cell in tagGenerator(first, filt=filtfunc):
-    cssclasses = cell.get("class")
+    cssclasses = cell.get("class") # interesting cells are identifiable by their CSS class:
     if cssclasses is not None and ('I' in cssclasses or 'E' in cssclasses):
-      booking = parseBooking(cell)
+      booking = processBooking(cell)
       if booking is not None:
         result.bookings.append(booking)
         continue
-    result.bookings.append(None)
+    result.bookings.append(None) # by default add None for this day
+
   if len(result.bookings) != 7:
     raise Exception("Error parsing week row, expected 7 days, got {n}".format(n=len(result.bookings)))
 
   return result
                        
 def processWeekPage(soup) :
+  """Parse the weekly timetable for a court, returning a list of
+  objects representing the 15-minute slots, in time order. Each object
+  contains the fields "time" and "bookings", which are respectively
+  the time (as a string) and a list of 7 objects representing the cell
+  content, either an Event object, a string, or None."""
   first_col_cell = soup.find('td', class_='red')
   if first_col_cell is None:
     raise Exception('Unable to parse - could not find first column in table')
@@ -67,19 +85,22 @@ def processWeekPage(soup) :
   first_data_row = first_col_cell.parent
   title_row = first_data_row.previous_sibling
   for row in tagGenerator(first_data_row):
-    data = parseWeekRow(row)
+    data = processWeekRow(row)
     slots.append(data)
   return slots
 
 def extractEvents(time_list, first_date, location):
-
+  """Process all of the information scraped from the page to produce a list of Event objects.
+  TIME_LIST is the list of 15-minute slots representing the court bookings for a week at that time.
+  FIRST_DATE is the date of the first column (which should be a Monday)
+  LOCATION is a description of the court"""
   quarterHour = datetime.timedelta(minutes=15)
 
   result = []
   for i in range(0,7):
     day_bookings = []
     result.append(day_bookings)
-    today = first_date + datetime.timedelta(days=i)
+    today = first_date + datetime.timedelta(days=i) # Week begins on Monday
     for j,row in enumerate(time_list):
       booking = row.bookings[i]
       if booking is not None:
@@ -88,7 +109,7 @@ def extractEvents(time_list, first_date, location):
           booking.duration = quarterHour
           booking.location = location
           day_bookings.append(booking)
-        elif isinstance(booking, unicode) and booking.strip() == '"':
+        elif isinstance(booking, unicode) and booking.strip() == '"': # Process dittos
           if len(day_bookings) == 0:
             raise Exception("encountered ditto before a booking at time {time} for day {i}".format(time=str(row.time), i=i))
           booking = day_bookings[-1] 
