5e9b3c74296f3e6a6293405a0a7009b649e4681c
Stu Perry <stewart.c.perry@gmail.com>
2014-09-25 18:46:49 +0100

    Fixed issues with merge and update. Also some cosmetic changes, minor refactoring and documentation/comments

--- scripts/Calendar.py
+++ scripts/Calendar.py
@@ -20,20 +20,24 @@ import oauth2client.file
 
 oauth2client.client.logger.setLevel(logging.DEBUG)
 
+# Credentials previously obtained from the google developers console...
 SERVICE_ACCOUNT_NAME='684425597639-rmr9jk9hrrbtt4uqs0v36cgfq5ksb77s@developer.gserviceaccount.com'
+PRIVATE_KEY_FILE=os.path.expanduser("~/etc/WSRC_Calendar-0d05fd27ef4c.p12")
+CREDENTIALS_CACHE_FILE=os.path.expanduser('~/etc/.credentials')
 CALENDAR_RW_SCOPE="https://www.googleapis.com/auth/calendar"
-MIDNIGHT = datetime.time(0, 0)
+
 UK_TIMEZONE = "Europe/London"
 UK_TZINFO = GBEireTimeZone()
 UTC_TZINFO = UTC()
 LOGGER = logging.getLogger(__name__)
 
 def _getService():
+  """Get and authenticated Google API service wrapper based on previously registered and stored credentials"""
   http = httplib2.Http()
-  storage = oauth2client.file.Storage(os.path.expanduser('~/etc/.credentials'))
+  storage = oauth2client.file.Storage(CREDENTIALS_CACHE_FILE)
   credentials = storage.get()
   if credentials is None or credentials.invalid == True:
-    fh = open(os.path.expanduser("~/etc/WSRC_Calendar-0d05fd27ef4c.p12"))
+    fh = open(PRIVATE_KEY_FILE)
     key = fh.read()
     fh.close()
     credentials = oauth2client.client.SignedJwtAssertionCredentials(service_account_name=SERVICE_ACCOUNT_NAME, private_key=key, scope=CALENDAR_RW_SCOPE)
@@ -41,8 +45,10 @@ def _getService():
   return apiclient.discovery.build('calendar', 'v3', http=http)
 
 class CalendarWrapper:
+  """Simple wrapper for a given calendar service"""
 
   def __init__(self, calendarId, service=_getService()):
+    """Create a new wrapper instance with the given calendar Id"""
     self.service = service
     self.calendarId = calendarId
     self.testing = False
@@ -75,9 +81,10 @@ class CalendarWrapper:
   def updateEvent(self, evt):
     LOGGER.info("Updating event {evt.name} [{evt.location}@{evt.time:%Y-%m-%d %H:%M:%S}]".format(**locals()))
     if not self.testing:
-      self.service.events().update(calendarId=self.calendarId, body=evt.googleEvent).execute()
+      self.service.events().update(calendarId=self.calendarId, eventId=evt.id, body=evt.toGoogleCalendarEvent()).execute()
     
 class Event:
+  "Simple wrapper for a calendar event. Has some utilities to convert to/from Google calendar event structures."
 
   def __init__(self, name, link, time=None, duration=None, location=None, id=None, googleEvent=None):
     self.name = name
@@ -89,7 +96,10 @@ class Event:
     self.googleEvent = googleEvent
 
   def toGoogleCalendarEvent(self):
-    return {
+    gevt = self.googleEvent
+    if gevt is None:
+      self.googleEvent = gevt = {}
+    gevt.update({
       'summary': self.name,
       'location': self.location,
       'id': self.id,
@@ -101,23 +111,18 @@ class Event:
         'dateTime': (self.time + self.duration).isoformat(),
         'timeZone': UK_TIMEZONE
         },
-      # 'attendees': [
-      #   {
-      #     'email': 'foo@bar.com',
-      #     # Other attendee's data...
-      #     },
-      #   ],
       'source': {
         'title': 'WSRC Booking System',
         'url': self.link
         }
-      }
+      })
+    return gevt
 
   @staticmethod
   def fromGoogleCalendarEvent(evt):
     def fromISODateTime(s):
       if not s.endswith("Z"):
-        raise Exception("Expected UTC time, unsure how to interpret")
+        raise Exception("Expected UTC time, unsure how to interpret: " + s)
       s = s[:-1]
       dt = datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S")
       dt = dt.replace(tzinfo=UTC_TZINFO)
@@ -136,20 +141,22 @@ class Event:
       id=evt["id"],
       googleEvent=evt)
 
-  def mergeIntoGoogleEvent(self, evt):
-    gevt = evt.googleEvent
-    gevt['summary'] = self.name
-    gevt['location'] = self.location,
-    gevt['start']['dateTime'] = self.time.isoformat(),
-    gevt['end']['dateTime'] = (self.time + self.duration).isoformat(),
-    gevt['source']['url'] = self.link
+  def mergeFrom(self, evt):
+    "Copy the details of the given event into this event"
+    # update all fields except Id - if this started as a google
+    # calendar event don't want to overwrite the id with a random one.
+    self.name = evt.name
+    self.link = evt.link
+    self.time = evt.time
+    self.duration = evt.duration
+    self.location = evt.location
 
   def identicalTo(self, other):
     mine,theirs = [(x.name, x.time, x.duration, x.link, x.location) for x in self, other]
     return mine == theirs
 
   def __key(self):
-      return (self.time, self.location)
+      return (self.time, self.location) # considered the same if start time and location are the same
 
   def __eq__(x, y):
       return x.__key() == y.__key()
