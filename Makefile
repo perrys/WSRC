2069bd6d43960848ec2df06127fb569ca6e6f08f
Stu Perry <stewart.c.perry@gmail.com>
2014-11-11 08:47:51 +0000

    fixed Makefile and setup.py

--- Makefile
+++ Makefile
@@ -1,41 +1,26 @@
-INSTALLDIR = /var/www
-
-INSTALL = install
-SED = sed
-
-IMG_SRCS = $(notdir $(wildcard resources/images/*))
-CSS_SRCS = $(notdir $(wildcard resources/css/*.css))
-JS_SRCS  = restful.js jquery.loadmask.js jquery.cookie.js webtoolkit_md5.js
-
-TARGETS = $(INSTALLDIR)/index.html $(INSTALLDIR)/wsrc/wsrc.wsgi $(INSTALLDIR)/wsrc/wsrc_server.py $(INSTALLDIR)/wsrc/lib/Database.py \
-	$(addprefix $(INSTALLDIR)/resources/js/,$(JS_SRCS)) \
-	$(addprefix $(INSTALLDIR)/resources/images/,$(IMG_SRCS)) \
-	$(addprefix $(INSTALLDIR)/resources/css/,$(CSS_SRCS)) \
+# -*- mode: makefile-gmake; -*-
 
 
-install: $(TARGETS)
+CSS_SRCS = resources/css/main.css resources/css/main_legacy.css
+JS_SRCS  = resources/js/main.js
 
-$(INSTALLDIR)/index.html: scripts/bracket_generator.py $(wildcard scripts/jinja-templates/*.html)
-	scripts/bracket_generator.py > $@
 
-resources/js/restful.js: resources/js/restful.coffee
+.PHONY: build install dist
+
+build: $(CSS_SRCS) $(JS_SRCS)
+
+install: build
+	python ./setup.py install
+
+bdist: build
+	python ./setup.py bdist
+
+resources/js/%.js: resources/js/%.coffee
 	coffee -c $<
 
-$(INSTALLDIR)/wsrc/%.py: %.py
-	$(INSTALL) -D $< $@
+resources/css/main.css: resources/css/_main.css
+	./tools/jinjac $< ie_mode=0 > $@
 
-$(INSTALLDIR)/wsrc/%.wsgi: %.wsgi
-	$(INSTALL) -d $(dir $@)
-	$(SED) s%@TARGETDIR%$(dir $@)% $< > $@
+resources/css/main_legacy.css: resources/css/_main.css
+	./tools/jinjac $< ie_mode=1 > $@
 
-$(INSTALLDIR)/wsrc/lib/%.py: lib/%.py
-	$(INSTALL) -D $< $@
-
-$(INSTALLDIR)/resources/images/%: resources/images/%
-	$(INSTALL) -D $< $@
-
-$(INSTALLDIR)/resources/css/%.css: resources/css/%.css
-	$(INSTALL) -D $< $@
-
-$(INSTALLDIR)/resources/js/%.js: resources/js/%.js
-	$(INSTALL) -D $< $@
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import glob
+import os.path
 
 setup(name='WSRC',
       version='1.0',
@@ -9,7 +11,14 @@ setup(name='WSRC',
       author_email='stewart.c.perry@gmail.com',
       maintainer_email='webmaster@wokingsquashclub.org',
       url='http://www.wokingsquashclub.org/',
-      packages=['wsrc', 'wsrc.booking', 'wsrc.utils', 'wsrc.site', 'wsrc.site.settings', 'wsrc.site.usermodel', 'wsrc.site.competitions'],
+      packages=['wsrc', 
+                'wsrc.booking', 
+                'wsrc.utils', 
+                'wsrc.site', 
+                'wsrc.site.settings', 
+                'wsrc.site.usermodel', 
+                'wsrc.site.competitions',
+                'wsrc.site.competitions.templatetags'],
       package_dir = {'wsrc': 'modules/wsrc',
                      'wsrc.booking': 'modules/wsrc/booking',
                      'wsrc.utils': 'modules/wsrc/utils',
@@ -17,9 +26,16 @@ setup(name='WSRC',
                      'wsrc.site.settings':  'modules/wsrc/site/settings',
                      'wsrc.site.usermodel': 'modules/wsrc/site/usermodel',
                      'wsrc.site.competitions':   'modules/wsrc/site/competitions',
+                     'wsrc.site.competitions.templatetags':   'modules/wsrc/site/competitions/templatetags',
                      },
-      package_data = {'wsrc': ['modules/wsrc/jinja2_templates/*.html', 'modules/wsrc/jinja2_templates/*.jinja2']},
+      package_data = {'wsrc': ['jinja2_templates/*.html', 'jinja2_templates/*.jinja2'],
+                      'wsrc.site': ['templates/*.html'],
+                      'wsrc.site.competitions': ['templates/*.html']
+                      },
       scripts = ['scripts/wsrc'],
       data_files = [('etc', ['etc/notifier.json', 'etc/smtp.json']),
+                    ('www/css', ['resources/css/main.css', 'resources/css/main_legacy.css']),
+                    ('www/js', ['resources/js/main.js']),
+                    ('www/images', glob.glob(os.path.join('resources', 'images', '*.jpg'))),
                     ]
      )
