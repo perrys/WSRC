1ec654e35c60b2a2d297546b740430c92350dea4
Stu Perry <stewart.c.perry@gmail.com>
2014-12-07 12:01:06 +0000

    move tournament renderer to separate class, with some refactoring

---
+++ modules/wsrc/site/competitions/tournament.py
@@ -0,0 +1,198 @@
+# This file is part of WSRC.
+#
+# WSRC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WSRC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WSRC.  If not, see <http://www.gnu.org/licenses/>.
+
+from wsrc.utils.html_table import Table, Cell, SpanningCell
+
+import xml.etree.ElementTree as etree
+
+NON_BRK_SPACE     = u'\xa0'
+
+TOP_LINK          = Cell('', {"class": "toplink"})
+BOTTOM_LINK       = Cell('', {"class": "bottomlink"})
+
+SETS_PER_MATCH    = 5
+COLS_PER_BRACKET  = 4 + SETS_PER_MATCH # + seeding, player/team name, and spacers
+
+HEADER_ROWS       = 3
+ROWS_PER_OPPONENT = 2 # we use rowspan=2 to allow "half" cell offsets
+ROWS_PER_MATCH    = 3 * ROWS_PER_OPPONENT
+
+def render_match(table, col, row, bracketIndex, matchIndex, idPrefix):
+
+    binomialId = (1<<bracketIndex) + matchIndex  
+    attrs = dict(locals())
+
+    class Position:
+        def __init__(self, col, row):
+            self.row = row
+            self.col = col
+
+    cursor = Position(col, row)
+
+    def addToRow(cls, content="", id=None):
+        attrs = {"class": cls}
+        if id is not None: attrs["id"] = id
+        c = SpanningCell(1, ROWS_PER_OPPONENT, unicode(content), attrs)
+        table.addCell(c, cursor.col, cursor.row)
+        cursor.col += c.ncols
+        return c
+
+    def addOpponent(isTop):
+        attrs["pos_identifier"] = isTop and 't' or 'b'
+        addToRow("seed empty-match ui-corner-%(pos_identifier)sl" % attrs, NON_BRK_SPACE)
+        addToRow("player empty-match", NON_BRK_SPACE, "match_%(idPrefix)s_%(binomialId)d_%(pos_identifier)s" % attrs)
+        for ii in range(0, SETS_PER_MATCH):
+            last = addToRow("score empty-match", NON_BRK_SPACE)
+        last.attrs["class"] += " ui-corner-%(pos_identifier)sr" % attrs
+
+    addOpponent(True)
+    cursor.col = col
+    cursor.row += ROWS_PER_OPPONENT
+    addOpponent(False)
+
+
+
+def render_bracket(table, nbrackets, bracketNumber, compressFirstRound, idPrefix, previousRowIndices = None):
+
+    isFirstRound = (bracketNumber == nbrackets)
+    isCompressedFirstRound = compressFirstRound and isFirstRound
+
+    if isCompressedFirstRound:
+        nmatches = 1 << (bracketNumber - 2)
+    else:
+        nmatches = 1 << (bracketNumber - 1)
+    column = 1 + (nbrackets-bracketNumber) * COLS_PER_BRACKET
+    
+    if previousRowIndices is None:
+        previousRowIndices = []
+        for j in range(0, nmatches):
+            pos = HEADER_ROWS + j * ROWS_PER_MATCH
+            previousRowIndices.append(pos)
+            previousRowIndices.append(pos)
+
+    rowIndices = []                    
+
+    for i in range(0, 2*nmatches, 2):
+        diff = previousRowIndices[i+1] - previousRowIndices[i]
+        avg  = previousRowIndices[i]   + diff / 2
+        rowIndices.append(avg)
+        if isCompressedFirstRound: 
+            matchIndex = i+1
+        else:
+            matchIndex = i/2
+        render_match(table, column, avg, bracketNumber-1, matchIndex, idPrefix)
+        if isFirstRound:
+            if compressFirstRound and not isCompressedFirstRound:
+                table.addCell(BOTTOM_LINK, column-2, avg+2)
+                table.addCell(BOTTOM_LINK, column-1, avg+2)
+        else:
+            table.addCell(TOP_LINK, column-2, previousRowIndices[i]+1)
+            table.addCell(BOTTOM_LINK, column-2, previousRowIndices[i+1]+2)
+            link = SpanningCell(1, diff, '', {"class": "stretchlink"})
+            table.addCell(link, column-2, previousRowIndices[i]+2)
+            table.addCell(TOP_LINK,        column-1, avg+1)
+            table.addCell(BOTTOM_LINK, column-1, avg+2)
+
+    return rowIndices
+
+
+def render_tournament(competition):
+    """Generate an html table showing an empty bracket, sized for the given competition."""
+
+    tournamentId = competition.id
+
+    # get the maximum match id
+    try:
+        maxId = reduce(max, [int(x.competition_match_id) for x in competition.match_set.all()], 0)
+    except Exception, e:
+        print 
+        for c in competition.match_set.all().order_by("competition_match_id"):
+            print c.__dict__
+        raise e
+    nbrackets = 1
+    while (maxId>>1) > 0:
+        nbrackets += 1
+        maxId = (maxId>>1)
+
+    # figure out the number of matches in the first round. If it is equal or less than half of the possible slots 
+    # then show the first round parallel with the second round
+    maxSecondRoundId = (1<<(nbrackets-1))-1
+    firstRoundMatches = competition.match_set.filter(competition_match_id__gt=maxSecondRoundId)
+    nFirstMatches = len(firstRoundMatches)
+    nSecondRoundMatches = (1 << (nbrackets-2))
+    compressFirstRound = (nFirstMatches <= nSecondRoundMatches)
+
+    rounds = dict([(r.round, r) for r in competition.rounds.all()])
+
+    ncols = nbrackets * COLS_PER_BRACKET - 1
+    if compressFirstRound:
+        nrows = HEADER_ROWS + (1 << nbrackets-2) * ROWS_PER_MATCH
+    else:
+        nrows = HEADER_ROWS + (1 << nbrackets-1) * ROWS_PER_MATCH
+
+    # Use our html table helper class to build up the html - allows us
+    # to just fill in the (row,colum) cells we want and it will take
+    # care of the blanks.
+    table = Table(ncols, nrows, {"class": "bracket", "cellspacing": "0px"})
+
+    # first row; nbsp in the top row
+    table.addCell(Cell("", None, True), 0, 0)
+    for i in range(nbrackets, 0, -1):
+        if i == 1:
+            content = "Final"
+        elif i == 2:
+            content = "Semi Finals"
+        elif i == 3:
+            content = "Quarter Finals"
+        else:
+            content = "Round %(i)d" % locals()
+        roundData = rounds.get(nbrackets-i+1)
+        if roundData is not None:
+            content += "\n[%s]" % roundData.end_date.strftime("%a %d %b")
+        table.addCell(SpanningCell(COLS_PER_BRACKET-2, 1, content, {"class": "roundtitle"}, True),    1+0+(nbrackets-i)*COLS_PER_BRACKET, 0)
+        if i > 1:
+            table.addCell(SpanningCell(1, 1, NON_BRK_SPACE, {"class": "leftspacer"}, True),  1 + COLS_PER_BRACKET-2 + (nbrackets-i)*COLS_PER_BRACKET, 0)
+            table.addCell(SpanningCell(1, 1, NON_BRK_SPACE, {"class": "rightspacer"}, True), 1 + COLS_PER_BRACKET-1 + (nbrackets-i)*COLS_PER_BRACKET, 0)
+
+    # first column; nbsp in the top row
+    for i in range(1, nrows):
+        content = NON_BRK_SPACE
+        if (i > 0): 
+            content = ""
+        attribs = {'class': "verticalspacer"}
+        if i == (nrows-1):
+            attribs["class"] += " spacercalc"        
+        table.addCell(Cell(content, attribs), 0, i)
+
+    # now draw the brackets
+    previousRowIndices = None
+    for i in range(nbrackets, 0, -1):
+        previousRowIndices = render_bracket(table, nbrackets, i, compressFirstRound, tournamentId, previousRowIndices)
+        if compressFirstRound and i == nbrackets:
+            previousRowIndices = None
+
+    # spacer calculation on last row:
+    col = 1
+    for i in range(nbrackets, 0, -1):
+        if col > 1:
+            table.addCell(SpanningCell(2, 1, '', {"class": "spacercalc"}), col, nrows-1) # links
+            col += 2
+        table.addCell(Cell('', {"class": "spacercalc"}), col, nrows-1) # seed
+        col += 2
+        table.addCell(SpanningCell(SETS_PER_MATCH, 1, '', {"class": "spacercalc"}), col, nrows-1) # links
+        col += SETS_PER_MATCH    
+
+    return etree.tostring(table.toHtml(), encoding='UTF-8', method='html')
+
--- modules/wsrc/site/competitions/views.py
+++ modules/wsrc/site/competitions/views.py
@@ -13,16 +13,18 @@
 # You should have received a copy of the GNU General Public License
 # along with WSRC.  If not, see <http://www.gnu.org/licenses/>.
 
-import rest_framework.filters
-import rest_framework.views as rest_views
-from django.shortcuts import get_object_or_404
-import rest_framework.generics as rest_generics
-import django.views.generic as view_generics
-from django.template.response import TemplateResponse
-
 from wsrc.site.usermodel.models import Player
 from wsrc.site.competitions.models import Competition, CompetitionGroup, Match
 from wsrc.site.competitions.serializers import PlayerSerializer, CompetitionSerializer, CompetitionGroupSerializer
+from wsrc.utils.timezones import parse_iso_date_to_naive
+
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+
+import rest_framework.filters
+import rest_framework.generics as rest_generics
+
+import tournament
 
 # REST data views:
 
@@ -62,24 +64,26 @@ class MatchDetail(rest_generics.RetrieveUpdateDestroyAPIView):
 class MatchCreate(rest_generics.CreateAPIView):
     model = Match
 
-NON_BRK_SPACE = u'\xa0'
-
 # HTML template views:
 
-def boxes_view(request, group_id):
-    """Return a view of the Leagues for identifier GROUP_ID. If
-    GROUP_ID is zero or negative, the nth-previous League set is
-    shown"""
+def get_competition_lists():
+    tournaments = []
+    for group in CompetitionGroup.objects.filter(comp_type="wsrc_tournaments"):
+        tournaments.append({"year": group.end_date.year, "competitions": group.competitions.all()})
+    return {
+        "tournaments": tournaments,
+        }
+
+def boxes_view(request, end_date=None):
+    """Return a view of the Leagues for ending on END_DATE. If
+    END_DATE is  negative, the current league is shown"""
 
     queryset = CompetitionGroup.objects
-    offset = None 
-    idx = int(group_id)
-    if idx < 1:
-        offset = - idx
-    if offset is not None:        
-        group = queryset.filter(comp_type="wsrc_boxes", active=True).order_by('-end_date')[offset]
+    if end_date is None:
+        group = queryset.filter(comp_type="wsrc_boxes", active=True).order_by('-end_date')[0]
     else:
-        group = get_object_or_404(queryset, id=group_id)
+        end_date = parse_iso_date_to_naive(end_date)
+        group = get_object_or_404(queryset, end_date=end_date)
 
     def create_box_config(previous_comp, competition, ctx):
         is_second = False
@@ -99,185 +103,29 @@ def boxes_view(request, group_id):
     nullplayer = {"name": "", "id": ""}
     last = None
     boxes = []
-    ctx = {"maxplayers": 0, "name": group.name, "id": group.id}
+    comp_meta = {"maxplayers": 0, "name": group.name, "id": group.id}
+    ctx = {"competition": comp_meta}
     for league in group.competitions.all():
-        cfg = create_box_config(last, league, ctx)
+        cfg = create_box_config(last, league, comp_meta)
         boxes.append(cfg)
         last = cfg
     for box in boxes:
-        while len(box["players"]) < ctx["maxplayers"]:
+        while len(box["players"]) < comp_meta["maxplayers"]:
             box["players"].append(nullplayer)
     ctx["boxes"] = boxes
-    return TemplateResponse(request, "boxes.html", {"league_config": ctx, "disable_ajax_links": True})
+    ctx.update(get_competition_lists())
+    return TemplateResponse(request, "boxes.html", ctx)
     
     
-
-def render_tournament(competition):
-
-    SETS_PER_MATCH=5
-    tournamentId = competition.id
-
-    # get the maximum match id
-    try:
-        maxId = reduce(max, [int(x.competition_match_id) for x in competition.match_set.all()], 0)
-    except Exception, e:
-        print 
-        for c in competition.match_set.all().order_by("competition_match_id"):
-            print c.__dict__
-        raise e
-    nbrackets = 1
-    while (maxId>>1) > 0:
-        nbrackets += 1
-        maxId = (maxId>>1)
-    # figure out the number of matches in the first round. If it is equal or less than half of the possible slots 
-    # then show the first round parallel with the second round
-    maxSecondRoundId = (1<<(nbrackets-1))-1
-    firstRoundMatches = competition.match_set.filter(competition_match_id__gt=maxSecondRoundId)
-    nFirstMatches = len(firstRoundMatches)
-    nSecondRoundMatches = (1 << (nbrackets-2))
-    compressFirstRound = (nFirstMatches <= nSecondRoundMatches)
-
-    rounds = dict([(r.round, r) for r in competition.rounds.all()])
-    from wsrc.utils.html_table import Table, Cell, SpanningCell
-
-    colsPerBracket = (4 + SETS_PER_MATCH)
-    ncols = nbrackets * colsPerBracket - 1
-    if compressFirstRound:
-        nrows = 3 + (1 << nbrackets-2) * 6
-    else:
-        nrows = 3 + (1 << nbrackets-1) * 6
-    table = Table(ncols, nrows, {"class": "bracket", "cellspacing": "0px"})
-
-    topLink        = Cell('', {"class": "toplink"})
-    bottomLink = Cell('', {"class": "bottomlink"})
-
-    # first row; nbsp in the top row
-    table.addCell(Cell("", None, True), 0, 0)
-    for i in range(nbrackets, 0, -1):
-        if i == 1:
-            content = "Final"
-        elif i == 2:
-            content = "Semi Finals"
-        elif i == 3:
-            content = "Quarter Finals"
-        else:
-            content = "Round %(i)d" % locals()
-        roundData = rounds.get(nbrackets-i+1)
-        if roundData is not None:
-            content += "\n[%s]" % roundData.end_date.strftime("%a %d %b")
-        table.addCell(SpanningCell(colsPerBracket-2, 1, content, {"class": "roundtitle"}, True),    1+0+(nbrackets-i)*colsPerBracket, 0)
-        if i > 1:
-            table.addCell(SpanningCell(1, 1, NON_BRK_SPACE, {"class": "leftspacer"}, True),    1 + colsPerBracket-2 + (nbrackets-i)*colsPerBracket, 0)
-            table.addCell(SpanningCell(1, 1, NON_BRK_SPACE, {"class": "rightspacer"}, True), 1 + colsPerBracket-1 + (nbrackets-i)*colsPerBracket, 0)
-
-    # first column; nbsp in the top row
-    for i in range(1, nrows):
-        content = NON_BRK_SPACE
-        if (i > 0): 
-            content = ""
-        attribs = {'class': "verticalspacer"}
-        if i == (nrows-1):
-            attribs["class"] += " spacercalc"        
-        table.addCell(Cell(content, attribs), 0, i)
-
-    def renderMatch(col, row, bracketIndex, matchIndex, idPrefix):
-    
-        binomialId = (1<<bracketIndex) + matchIndex    
-        class Position:
-            def __init__(self, col, row):
-                self.row = row
-                self.col = col
-
-        p = Position(col, row)
-
-        def addToRow(cls, content="", id=None):
-            attrs = {"class": cls}
-            if id is not None: attrs["id"] = id
-            c = SpanningCell(1, 2, unicode(content), attrs)
-            table.addCell(c, p.col, p.row)
-            p.col += c.ncols
-            return c
-        addToRow("seed ui-corner-tl", NON_BRK_SPACE)
-        addToRow("player", NON_BRK_SPACE, "match_%(idPrefix)s_%(binomialId)d_t" % locals())
-        for ii in range(0, SETS_PER_MATCH):
-            last = addToRow("score", NON_BRK_SPACE)
-        last.attrs["class"] += " ui-corner-tr"
-
-        p.col = col
-        p.row += 2
-        addToRow("seed ui-corner-bl", NON_BRK_SPACE)
-        addToRow("player", NON_BRK_SPACE, "match_%(idPrefix)s_%(binomialId)d_b" % locals())
-        for ii in range(0, SETS_PER_MATCH):
-            last = addToRow("score", NON_BRK_SPACE)
-        last.attrs["class"] += " ui-corner-br"
-
-    def renderBracket(bracketNumber, idPrefix, previousRowIndices = None):
-        isCompressedFirstRound = compressFirstRound and bracketNumber == nbrackets
-        if isCompressedFirstRound:
-            nmatches = 1 << (bracketNumber - 2)
-        else:
-            nmatches = 1 << (bracketNumber - 1)
-        column = 1+(nbrackets-bracketNumber)*colsPerBracket
-        
-        firstRound = (previousRowIndices is None)
-        if firstRound:
-            previousRowIndices = []
-            for j in range(0, nmatches):
-                pos = 3 + j * 6
-                previousRowIndices.append(pos)
-                previousRowIndices.append(pos)
-
-        rowIndices = []                    
-
-        for i in range(0, 2*nmatches, 2):
-            diff = previousRowIndices[i+1] - previousRowIndices[i]
-            avg    = previousRowIndices[i] + diff / 2
-            rowIndices.append(avg)
-            if isCompressedFirstRound: 
-                matchIndex = i+1
-            else:
-                matchIndex = i/2
-            renderMatch(column, avg, bracketNumber-1, matchIndex, idPrefix)
-            if firstRound:
-                if compressFirstRound and not isCompressedFirstRound:
-                    table.addCell(bottomLink, column-2, avg+2)
-                    table.addCell(bottomLink, column-1, avg+2)
-            else:
-                table.addCell(topLink, column-2, previousRowIndices[i]+1)
-                table.addCell(bottomLink, column-2, previousRowIndices[i+1]+2)
-                link = SpanningCell(1, diff, '', {"class": "stretchlink"})
-                table.addCell(link, column-2, previousRowIndices[i]+2)
-                table.addCell(topLink,        column-1, avg+1)
-                table.addCell(bottomLink, column-1, avg+2)
-
-        return rowIndices
-
-    previousRowIndices = None
-    for i in range(nbrackets, 0, -1):
-        previousRowIndices = renderBracket(i, tournamentId, previousRowIndices)
-        if compressFirstRound and i == nbrackets:
-            previousRowIndices = None
-
-    # spacer calculation on last row:
-    col = 1
-    for i in range(nbrackets, 0, -1):
-        if col > 1:
-            table.addCell(SpanningCell(2, 1, '', {"class": "spacercalc"}), col, nrows-1) # links
-            col += 2
-        table.addCell(Cell('', {"class": "spacercalc"}), col, nrows-1) # seed
-        col += 2
-        table.addCell(SpanningCell(SETS_PER_MATCH, 1, '', {"class": "spacercalc"}), col, nrows-1) # links
-        col += SETS_PER_MATCH    
-
-    return table
 
 def bracket_view(request, year, name):
-    import xml.etree.ElementTree as etree
     group = get_object_or_404(CompetitionGroup.objects, end_date__year=year, comp_type='wsrc_tournaments')
     competition = get_object_or_404(group.competitions, name__iexact=name)
 
-    table = render_tournament(competition)
-    html = etree.tostring(table.toHtml(), encoding='UTF-8', method='html')
+    html_table = tournament.render_tournament(competition)
+
+    ctx = {"competition": competition, "bracket": html_table}
+    ctx.update(get_competition_lists())
     
-    return TemplateResponse(request, "tournaments.html", {"competition": competition, "bracket": html})
+    return TemplateResponse(request, "tournaments.html", ctx)
     
