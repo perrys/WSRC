9be666d3156bd22376eeea4d29bd2430bc8dcac6
Stu Perry <stewart.c.perry@gmail.com>
2014-12-07 11:59:38 +0000

    consolidated competition urls

--- modules/wsrc/site/settings/urls.py
+++ modules/wsrc/site/settings/urls.py
@@ -15,17 +15,20 @@ import wsrc.site.competitions.views
 urlpatterns = patterns('',
 
     url(r'^$', wsrc.site.views.index_view, name="homepage"),
-    url(r'^home$', wsrc.site.views.index_view),
-    url(r'^index$', wsrc.site.views.index_view),
+    url(r'^home/?$', wsrc.site.views.index_view),
+    url(r'^index/?$', wsrc.site.views.index_view),
+
+    url(r'^login/?$', django.contrib.auth.views.login, {'template_name': 'login.html'}),
+    url(r'^logout/?$', django.contrib.auth.views.logout),
+
+    url(r'^boxes/(\d{4}-\d{2}-\d{2})/?$', wsrc.site.competitions.views.boxes_view), # end-date based
+    url(r'^boxes/?$',                    wsrc.site.competitions.views.boxes_view), # most recent
+
+    url(r'^tournament/(\d{4})/(\w+)/?$', wsrc.site.competitions.views.bracket_view),
+
     url(r'^data/facebook$', wsrc.site.views.facebook_view),
+    url(r'^data/',    include(wsrc.site.competitions.data_urls)),
 
-    url(r'^login$', django.contrib.auth.views.login, {'template_name': 'login.html'}),
-    url(r'^logout$', django.contrib.auth.views.logout),
-
-    url(r'^tournament/(\d{4})/(\w+)$', wsrc.site.competitions.views.bracket_view),
-
-    url(r'^comp_data/',    include(wsrc.site.competitions.data_urls)),
-    url(r'^competitions/', include(wsrc.site.competitions.view_urls)),
     url(r'^tournaments/',  include(wsrc.site.competitions.legacy_urls)),
 
     url(r'^admin/', include(admin.site.urls)),
