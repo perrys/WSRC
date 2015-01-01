1069df3e61642b95661358e76d2f75ead3ce725d
Stu Perry <stewart.c.perry@gmail.com>
2015-01-01 19:25:45 +0000

    add reverse url names and 301 redirects for old site urls

--- modules/wsrc/site/settings/urls.py
+++ modules/wsrc/site/settings/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 import wsrc.site.competitions.data_urls
 import wsrc.site.competitions.view_urls
@@ -13,37 +14,55 @@ import wsrc.site.views
 import wsrc.site.competitions.views
 import wsrc.site.usermodel.views
 
+def perm_redirect(view, permanent=True):
+  def func(request):
+    return redirect(view, permanent=permanent)
+  return func 
+
 urlpatterns = patterns('',
 
-    url(r'^$', wsrc.site.views.index_view, name="homepage"),
-    url(r'^home/?$', wsrc.site.views.index_view),
-    url(r'^index/?$', wsrc.site.views.index_view),
-    url(r'^memberlist/?$', wsrc.site.usermodel.views.MemberListView.as_view()),
 
-    url(r'^login/?$', auth_views.login, {'template_name': 'login.html'}),
-    url(r'^logout/?$', auth_views.logout),
+    url(r'^$',       wsrc.site.views.index_view),
+    url(r'^home/?$', wsrc.site.views.index_view, name="homepage"),
+
+    url(r'^login/?$',  auth_views.login, {'template_name': 'login.html'}),
+    url(r'^logout/?$', auth_views.logout, name='logout'),
+
     url(r'^password/reset/$',auth_views.password_reset,name='password_reset'),
     url(r'^password/reset/done/$',auth_views.password_reset_done,name='password_reset_done'),
     url(r'^password/reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^password/reset/complete/$',auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^password/change/$', auth_views.password_change, name='password_change'),
     url(r'^password/change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^change_password/?$', wsrc.site.views.change_password_view),
+
+    url(r'^memberlist/?$', wsrc.site.usermodel.views.MemberListView.as_view(), name="member_list"),
 
     url(r'^boxes/(\d{4}-\d{2}-\d{2})/?$', wsrc.site.competitions.views.boxes_view), # end-date based
-    url(r'^boxes/?$',                    wsrc.site.competitions.views.boxes_view), # most recent
+    url(r'^boxes/?$',                     wsrc.site.competitions.views.boxes_view, name="boxes"), # most recent
 
     url(r'^tournament/(\d{4})/(\w+)/?$', wsrc.site.competitions.views.bracket_view),
-    url(r'^tournament/?', wsrc.site.competitions.views.bracket_view, {"year":None, "name":"Open"}),
+    url(r'^tournament/?',                wsrc.site.competitions.views.bracket_view, {"year":None, "name":"Open"} , name="tournaments"),
 
-    url(r'^settings/?$', wsrc.site.views.settings_view),
-    url(r'^change_password/?$', wsrc.site.views.change_password_view),
+    url(r'^settings/?$', wsrc.site.views.settings_view, name="settings"),
 
     url(r'^data/facebook$', wsrc.site.views.facebook_view),
     url(r'^data/',    include(wsrc.site.competitions.data_urls)),
 
-    url(r'^tournaments/',  include(wsrc.site.competitions.legacy_urls)),
-
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^html/club_history.html',   perm_redirect('/about')),
+    url(r'^html/membership.html',     perm_redirect('/membership')),
+    url(r'^html/how_to_find_us.html', perm_redirect('/contact')),
+    url(r'^html/club_nights.html',    perm_redirect('/about')),
+    url(r'^html/coaching.html',       perm_redirect('/coaching')),
+    url(r'^html/juniors.html',        perm_redirect('/juniors')),
+    url(r'^html/teams.html',          perm_redirect('/home')),
+    url(r'^html/events.html',         perm_redirect('/home')),
+    url(r'^html/news.php',            perm_redirect('/home')),
+    url(r'^html/links.html',          perm_redirect('/links')),
+    url(r'^tournaments/index.html',   perm_redirect('/tournament')),
+
 
     url(r'^(?P<page>[a-z_]+)$', wsrc.site.views.generic_view),
 )
