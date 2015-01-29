bba063595cc946a266852405d5d3d23df718a9c1
Stu Perry <stewart.c.perry@gmail.com>
2015-01-29 00:20:15 +0000

    ensure logging is configured

--- modules/wsrc/site/settings/wsgi.py
+++ modules/wsrc/site/settings/wsgi.py
@@ -10,5 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wsrc.site.settings.settings")
 
+import logging
+logging.basicConfig(format='%(asctime)-10s [%(levelname)s] %(message)s',datefmt="%Y-%m-%d %H:%M:%S")
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
