aa28c82e9df0d3cbada671241148c659a65f51f4
Stu Perry <stewart.c.perry@gmail.com>
2015-02-07 21:08:00 +0000

    added information from membership spreadsheet to Player model - membership ID (cardnumber), membership type, do not receive email

--- modules/wsrc/site/usermodel/models.py
+++ modules/wsrc/site/usermodel/models.py
@@ -8,14 +8,42 @@ from django.utils.translation import ugettext_lazy as _
 import re
 
 class Player(models.Model):
+
+  MEMBERSHIP_TYPES = (
+    ("coach", "Coach"),
+    ("compl", "Complementary"),
+    ("full", "Full"),
+    ("junior", "Junior"),
+    ("non_play", "Social"),
+    ("off_peak", "Off-Peak"),
+    ("y_adult", "Young Adult"),
+    )
+
   user = models.OneToOneField(User)
 
-  phone_validator = validators.RegexValidator(re.compile('^\+?[\d ]+$'), _('Enter a valid phone number.'), 'invalid')
+  phone_validator = validators.RegexValidator(re.compile('^\+?[\d ]+$'), ('Enter a valid phone number.'), 'invalid')
 
-  cell_phone  = models.CharField(_('mobile phone'), max_length=30, validators = [phone_validator], blank=True)
-  other_phone = models.CharField(_('other phone'), max_length=30, validators = [phone_validator], blank=True)
-  short_name  = models.CharField(_('short name'), max_length=32, blank=True)
-  squashlevels_id  = models.IntegerField(db_index=True, blank=True, null=True)
+  cell_phone  = models.CharField(('mobile phone'), max_length=30, validators = [phone_validator], blank=True)
+  other_phone = models.CharField(('other phone'), max_length=30, validators = [phone_validator], blank=True)
+  short_name  = models.CharField(('short name'), max_length=32, blank=True)
+
+  membership_type = models.CharField(max_length=8, choices=MEMBERSHIP_TYPES,
+                                         verbose_name="Membership Type"
+                                     )
+  membership_id  = models.IntegerField(db_index=True, blank=True, null=True,
+                                         help_text="Your membership ID - this is normally the number on your door entry card",
+                                         verbose_name="Membership ID"
+                                       )
+  squashlevels_id  = models.IntegerField(db_index=True, blank=True, null=True, 
+                                         help_text="ID on the squashlevels website - it is not normally necessary to change this",
+                                         verbose_name="SquashLevels ID"
+                                         )
+  prefs_receive_email  = models.BooleanField(default=True,
+                                             null=True,
+                                             blank=True,
+                                             help_text="Uncheck if you do *not* want to receive emails from the club&emdash; match reminders, social events etc.",
+                                             verbose_name="Receive Email"
+                                             )
 
 #  def get_absolute_url(self):
 #      return "/users/%s/" % urlquote(self.user.email)
