9de771c9d8486268e427068a7fadb36a9afaf347
Stu Perry <stewart.c.perry@gmail.com>
2015-03-03 19:11:58 +0000

    if WSRC is not defined, store page for defered event call

--- resources/js/pre_jqm.js
+++ resources/js/pre_jqm.js
@@ -1,5 +1,8 @@
 $(document).on( "mobileinit", function() {
   $(document).on( "pagecontainershow", function( event, ui ) {
-    WSRC.onPageContainerShow(event, ui)
+    if (window.WSRC === undefined)
+      window.WSRC_deferedPageContainerShow = [event, ui]
+    else
+      WSRC.onPageContainerShow(event, ui)
   });
 });
