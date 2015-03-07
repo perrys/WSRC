7cd6813572d7c7f7fbe475e1ba59f63557cafc22
Stu Perry <stewart.c.perry@gmail.com>
2015-03-07 23:19:52 +0000

    send simple email message if html body not supplied

--- modules/wsrc/utils/email_utils.py
+++ modules/wsrc/utils/email_utils.py
@@ -7,7 +7,7 @@ import time
 import traceback
 import unittest
 
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMessage, EmailMultiAlternatives
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
@@ -18,9 +18,13 @@ def send_email(subject, text_body, html_body, from_address, to_list, bcc_list=No
   headers = {}
   if reply_to_address is not None:
     headers['Reply-To'] = reply_to_address
-  msg = EmailMultiAlternatives(subject, text_body, from_address,
-                               to_list, bcc_list, headers)
-  msg.attach_alternative(html_body, "text/html")
+  if html_body is not None:
+    msg = EmailMultiAlternatives(subject, text_body, from_address,
+                                 to_list, bcc_list, headers=headers)
+    msg.attach_alternative(html_body, "text/html")
+  else
+    msg = EmailMessage(subject, text_body, from_address,
+                       to_list, bcc_list, headers=headers)
   msg.send(fail_silently=False)
 
 def send_markdown_email(subject, markdown_body, from_address, to_list, bcc_list=None, reply_to_address=None):
