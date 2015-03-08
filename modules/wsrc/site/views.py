74b013673d6475f228480e5bf5f79816e270b104
Stu Perry <stewart.c.perry@gmail.com>
2015-03-08 16:04:27 +0000

    added test/debug code path

--- modules/wsrc/site/views.py
+++ modules/wsrc/site/views.py
@@ -244,10 +244,16 @@ class SendEmail(APIView):
         elif fmt == 'html':
             email_data['text_body'] = ''
             email_data['html_body'] = body
-        import pprint
-        print pprint.pprint(email_data)
-        import wsrc.utils.email_utils as email_utils
-        email_utils.send_email(**email_data)
+        debug = False
+        if debug:
+          import pprint, time
+          print pprint.pprint(email_data)
+          time.sleep(1)
+          if 'stewart.c.perry@gmail.com' in email_data['bcc_list']:
+              return HttpResponse("Invalid email address", content_type="text/plain", status=403)
+        else:
+          import wsrc.utils.email_utils as email_utils
+          email_utils.send_email(**email_data)
         return HttpResponse(status=204)
 
 class UserForm(ModelForm):
