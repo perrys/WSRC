df0eb8a201c1c47299910999e8a1dc1bcbc2d4bc
Stu Perry <stewart.c.perry@gmail.com>
2015-04-12 12:04:42 +0100

    admin for Transactions

--- modules/wsrc/site/accounts/admin.py
+++ modules/wsrc/site/accounts/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Transaction
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "description", "regex")
     list_editable = ("name", "description", "regex",)
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("account", "date_issued", "date_cleared", "amount", "bank_memo", "comment", "category")
+    list_editable = ("date_cleared", "comment", "category",)
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Transaction, TransactionAdmin)
