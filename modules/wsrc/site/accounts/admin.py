787b256e3762c13736958066075a965e90f9d734
Stu Perry <stewart.c.perry@gmail.com>
2015-05-22 06:25:40 +0100

    make account editable in transaction list view

--- modules/wsrc/site/accounts/admin.py
+++ modules/wsrc/site/accounts/admin.py
@@ -7,8 +7,8 @@ class CategoryAdmin(admin.ModelAdmin):
     list_editable = ("name", "ordering", "description", "regex", "is_reconciling",)
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("account", "date_issued", "date_cleared", "bank_number", "amount", "bank_memo", "comment", "category")
-    list_editable = ("date_cleared", "comment", "category",)
+    list_display = ("date_issued", "date_cleared", "account", "bank_number", "amount", "bank_memo", "comment", "category")
+    list_editable = ("date_cleared", "account", "comment", "category",)
     list_filter = ('account', 'category')
 
 admin.site.register(Account)
