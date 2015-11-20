40f4b2a95946c6057dc5b24e3105b118705838ea
Stu Perry <stewart.c.perry@gmail.com>
2015-11-20 18:53:58 +0000

    added django initializer code

--- modules/wsrc/external_sites/__init__.py
+++ modules/wsrc/external_sites/__init__.py
@@ -1,1 +1,14 @@
 
+def init():
+
+  import os
+  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wsrc.site.settings.settings")
+
+  import logging
+  logging.basicConfig(format='%(asctime)-10s [%(levelname)s] %(message)s',datefmt="%Y-%m-%d %H:%M:%S")
+
+  import django
+  if hasattr(django, "setup"):
+    django.setup()
+
+init()
