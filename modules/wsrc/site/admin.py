d1ab39f903985b9dd111621ce71065a462b28070
Stu Perry <stewart.c.perry@gmail.com>
2015-11-30 09:12:05 +0000

    admin for maintenance issues

--- modules/wsrc/site/admin.py
+++ modules/wsrc/site/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db import models
 
 from django.contrib import admin
-from wsrc.site.models import PageContent, EmailContent, EventFilter
+from wsrc.site.models import PageContent, EmailContent, EventFilter, MaintenanceIssue
 
 class PageContentAdmin(admin.ModelAdmin):
     formfield_overrides = {
@@ -17,7 +17,15 @@ class EmailContentAdmin(admin.ModelAdmin):
 class NotifierEventAdmin(admin.ModelAdmin):
     pass
 
+class MaintenanceIssueAdmin(admin.ModelAdmin):
+    list_display = ("description", "reporter", "reported_date", "status",)
+    list_filter = ('status',)
+    formfield_overrides = {
+        models.TextField: {'widget': forms.Textarea(attrs={'cols': 100, 'rows': 5})},
+    }
+
 
 admin.site.register(PageContent, PageContentAdmin)
 admin.site.register(EmailContent, EmailContentAdmin)
 admin.site.register(EventFilter, NotifierEventAdmin)
+admin.site.register(MaintenanceIssue, MaintenanceIssueAdmin)
