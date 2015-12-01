61259e2c80df3031c3484e01dc8458b62cb08034
Stu Perry <stewart.c.perry@gmail.com>
2015-12-01 09:04:08 +0000

    word trimming utility

--- modules/wsrc/utils/text.py
+++ modules/wsrc/utils/text.py
@@ -25,3 +25,8 @@ def plural(l, extra=""):
     return ""
   return extra + "s"
 
+def shorten(text, max_words):
+  words = text.split(" ")
+  if len(words) > max_words:
+    return " ".join(words[:max_words]) + "..."
+  return text
