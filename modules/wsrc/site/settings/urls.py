e8c9f7ce40214855a4b0de9eccf405ebaa6d2c96
Stu Perry <stewart.c.perry@gmail.com>
2016-01-13 00:11:53 +0000

    add leagues admin page (WIP)

--- modules/wsrc/site/settings/urls.py
+++ modules/wsrc/site/settings/urls.py
@@ -44,6 +44,7 @@ urlpatterns = patterns('',
 
     url(r'^boxes/(\d{4}-\d{2}-\d{2})/?$', wsrc.site.competitions.views.boxes_view), # end-date based
     url(r'^boxes/?$',                     wsrc.site.competitions.views.boxes_view, name="boxes"), # most recent
+    url(r'^boxes/admin?$',                wsrc.site.competitions.views.boxes_view, {'template_name': 'boxes_admin.html'}),
 
     url(r'^tournaments/admin/(\d{4})/([\w\s]+)/?',  wsrc.site.competitions.views.bracket_admin_view),
     url(r'^tournaments/admin/?',                    wsrc.site.competitions.views.bracket_admin_view),
