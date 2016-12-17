630acb90d1a3c3f7e8edad1ff932d8b73459d80f
Stu Perry <stewart.c.perry@gmail.com>
2016-12-17 10:18:02 +0000

    enhanced list display, edit, filtering for user model, added booking system ID

--- modules/wsrc/site/usermodel/admin.py
+++ modules/wsrc/site/usermodel/admin.py
@@ -13,6 +13,19 @@ class UserProfileInline(admin.StackedInline):
 
 class UserAdmin(AuthUserAdmin):
  inlines = AuthUserAdmin.inlines + [UserProfileInline,]
+ list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'membership_type', 'booking_system_id')
+ list_editable = ("email", "first_name", "last_name")
+
+ list_filter = ('player__membership_type', 'is_active', 'is_staff', 'groups', 'is_superuser')
+ ordering = ('username', 'first_name', 'last_name')
+
+ def booking_system_id(self, obj):
+  return obj.player.booking_system_id
+ booking_system_id.short_description = "Booking Site ID"
+
+ def membership_type(self, obj):
+  return obj.player.membership_type
+ membership_type.short_description = "Membership Category"
 
 # unregister old user admin
 admin.site.unregister(User)
