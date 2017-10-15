9fcaf5f56059e86eb6c50873811e217681ae3520
Stu Perry <stewart.c.perry@gmail.com>
2017-10-15 02:58:45 +0100

    Exclude people with "don't display" preference from member list

--- modules/wsrc/site/usermodel/views.py
+++ modules/wsrc/site/usermodel/views.py
@@ -38,7 +38,10 @@ class MemberListView(ListView):
         return super(MemberListView, self).dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-      return Player.objects.values("id", "user__first_name", "user__last_name", "user__email", "other_phone", "cell_phone", "user__is_active").filter(user__is_active=True).order_by('user__first_name', 'user__last_name')
+      return Player.objects.values("id", "user__first_name", "user__last_name", "user__email", "other_phone", "cell_phone", "user__is_active") \
+                           .filter(user__is_active=True) \
+                           .exclude(prefs_display_contact_details=False) \
+                           .order_by('user__first_name', 'user__last_name')
 
     def get_template_names(self):
       return ["memberlist.html"]
