90b7f65f8b41191c2de938138d4569c26903c200
Stu Perry <stewart.c.perry@gmail.com>
2017-10-31 18:38:42 +0000

    Optimise list display related object DB queries

--- modules/wsrc/site/accounts/admin.py
+++ modules/wsrc/site/accounts/admin.py
@@ -8,8 +8,12 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ("date_issued", "date_cleared", "account", "bank_number", "amount", "bank_memo", "comment", "category")
-    list_editable = ("date_cleared", "account", "comment", "category",)
+#    list_editable = ("date_cleared", "account", "comment", "category",)
     list_filter = ('account', 'category')
+    def get_queryset(self, request):
+        qs = super(TransactionAdmin, self).get_queryset(request)
+        qs = qs.select_related('category', 'account')
+        return qs
 
 admin.site.register(Account)
 admin.site.register(Category, CategoryAdmin)
