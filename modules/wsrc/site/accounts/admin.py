1376280fa7f18b734cd86ced06b92ff87fb4478b
Stu Perry <stewart.c.perry@gmail.com>
2017-11-04 20:51:19 +0000

    Add inline subscription payment form; use form_utils mixins

--- modules/wsrc/site/accounts/admin.py
+++ modules/wsrc/site/accounts/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 
 from .models import Account, Category, Transaction
+from wsrc.site.usermodel.models import SubscriptionPayment
+from wsrc.utils.form_utils import SelectRelatedForeignFieldMixin, SelectRelatedQuerysetMixin
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "ordering", "description", "regex", "is_reconciling",)
     list_editable = ("name", "ordering", "description", "regex", "is_reconciling",)
 
-class TransactionAdmin(admin.ModelAdmin):
+class SubscriptionPaymentInline(SelectRelatedForeignFieldMixin, SelectRelatedQuerysetMixin, admin.StackedInline):
+    model = SubscriptionPayment
+    max_num = 1
+    can_delete = True
+    
+class TransactionAdmin(SelectRelatedQuerysetMixin, admin.ModelAdmin):
+    inlines = (SubscriptionPaymentInline,)
     list_display = ("date_issued", "date_cleared", "account", "bank_number", "amount", "bank_memo", "comment", "category")
 #    list_editable = ("date_cleared", "account", "comment", "category",)
     list_filter = ('account', 'category')
-    def get_queryset(self, request):
-        qs = super(TransactionAdmin, self).get_queryset(request)
-        qs = qs.select_related('category', 'account')
-        return qs
 
 admin.site.register(Account)
 admin.site.register(Category, CategoryAdmin)
