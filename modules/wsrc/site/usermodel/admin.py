bed92b1985881fb3f5327811958b6ebfda8f308c
Stu Perry <stewart.c.perry@gmail.com>
2017-11-04 20:57:08 +0000

    Add admins for Season and Subscription - DB optimized

--- modules/wsrc/site/usermodel/admin.py
+++ modules/wsrc/site/usermodel/admin.py
@@ -1,10 +1,15 @@
+from django import forms
+from django.db import models
+
 from django.contrib import admin
 
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
-from wsrc.site.usermodel.models import Player
+from wsrc.site.accounts.models import Transaction
+from wsrc.site.usermodel.models import Player, Season, Subscription, SubscriptionPayment
+from wsrc.utils.form_utils import SelectRelatedForeignFieldMixin, SelectRelatedQuerysetMixin, CachingModelChoiceField, get_related_field_limited_queryset
 
 class UserProfileInline(admin.StackedInline):
  model = Player
@@ -48,3 +53,50 @@ class UserAdmin(AuthUserAdmin):
 admin.site.unregister(User)
 # register new user admin
 admin.site.register(User, UserAdmin)
+
+class SeasonAdmin(admin.ModelAdmin):
+  list_display = ('__unicode__', "has_ended")
+  list_editable = ("has_ended",)
+
+class SubscriptionForm(forms.ModelForm):
+  player = forms.ModelChoiceField(queryset=get_related_field_limited_queryset(Subscription.player.field).select_related("user"))  
+
+class SubscriptionPaymentForm(forms.ModelForm):
+  transaction = CachingModelChoiceField(queryset=get_related_field_limited_queryset(SubscriptionPayment.transaction.field))
+    
+class SubscriptionPaymentInline(SelectRelatedQuerysetMixin, admin.StackedInline):
+  model = SubscriptionPayment
+  can_delete = True
+  form = SubscriptionPaymentForm
+    
+class SubscriptionAdmin(admin.ModelAdmin):
+  inlines = (SubscriptionPaymentInline,)
+  list_display = ('player', 'season', 'membership_type', 'payment_frequency', 'payments_count', 'total_payments', 'signed_off', "comment")
+  list_filter = ('signed_off', 'payment_frequency', 'player__membership_type', )
+  list_editable = ('signed_off', 'comment')
+  formfield_overrides = {
+    models.TextField: {'widget': forms.Textarea(attrs={'cols': 30, 'rows': 1})},
+  }
+  form = SubscriptionForm
+
+  def subscription(self, obj):
+    return unicode(obj)
+  subscription.admin_order_field = "player__user__first_name"
+  
+  def total_payments(self, obj):
+    return obj.get_total_payments()
+  total_payments.short_description = "Total"
+
+  def membership_type(self, obj):
+    return obj.player.membership_type 
+  membership_type.short_description = "Membership Type"
+  membership_type.admin_order_field = "player__membership_type"
+   
+  def get_queryset(self, request):
+    qs = super(SubscriptionAdmin, self).get_queryset(request)
+    qs = qs.select_related('player__user', 'season')
+    qs = qs.prefetch_related('payments__transaction')
+    return qs
+
+admin.site.register(Season, SeasonAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
