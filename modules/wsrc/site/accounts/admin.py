71395c0e4d1c5e5763ef616bb0aedb1c5dd974d4
Stu Perry <stewart.c.perry@gmail.com>
2017-11-06 07:45:42 +0000

    Add subscription field to list display; per_page=200

--- modules/wsrc/site/accounts/admin.py
+++ modules/wsrc/site/accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 from .models import Account, Category, Transaction
-from wsrc.site.usermodel.models import SubscriptionPayment
-from wsrc.utils.form_utils import SelectRelatedForeignFieldMixin, SelectRelatedQuerysetMixin
+from wsrc.site.usermodel.models import SubscriptionPayment, Subscription
+from wsrc.utils.form_utils import SelectRelatedForeignFieldMixin, SelectRelatedQuerysetMixin, PrefetchRelatedQuerysetMixin
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "ordering", "description", "regex", "is_reconciling",)
@@ -13,11 +13,22 @@ class SubscriptionPaymentInline(SelectRelatedForeignFieldMixin, SelectRelatedQue
     max_num = 1
     can_delete = True
     
-class TransactionAdmin(SelectRelatedQuerysetMixin, admin.ModelAdmin):
+class TransactionAdmin(PrefetchRelatedQuerysetMixin, admin.ModelAdmin):
     inlines = (SubscriptionPaymentInline,)
-    list_display = ("date_issued", "date_cleared", "account", "bank_number", "amount", "bank_memo", "comment", "category")
-#    list_editable = ("date_cleared", "account", "comment", "category",)
+    list_display = ("date_issued", "date_cleared", "account", "bank_number", "amount", "bank_memo", "category", "subscription", "comment")
+    list_editable = ("comment",)
     list_filter = ('account', 'category')
+    list_per_page = 200
+    list_select_related = True
+    prefetch_related_fields = ('subs_payments',)
+    def subscription(self, obj):
+        subs_payments = obj.subs_payments
+        for sp in subs_payments.all():
+            cache = getattr(self, 'subscription_cache', None)
+            if cache is None:
+                self.subscription_cache = cache = dict([(sub.pk, sub) for sub in Subscription.objects.all()])
+            return unicode(cache.get(sp.subscription_id))
+        return None
 
 admin.site.register(Account)
 admin.site.register(Category, CategoryAdmin)
