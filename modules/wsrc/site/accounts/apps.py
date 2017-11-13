def7c12c2742d36cd3730945b93a0172cd2bcea3
Stu Perry <stewart.c.perry@gmail.com>
2017-11-13 07:42:28 +0000

    Change admin app title to "Finances"

---
+++ modules/wsrc/site/accounts/apps.py
@@ -0,0 +1,23 @@
+# This file is part of WSRC.
+#
+# WSRC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WSRC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WSRC.  If not, see <http://www.gnu.org/licenses/>.
+
+"App config for the accounts models"
+
+from django.apps import AppConfig
+
+class AccountsModelAppConfig(AppConfig):
+    "Sets the admin title to a sensible name"
+    name = 'wsrc.site.accounts'
+    verbose_name = 'Finances'
