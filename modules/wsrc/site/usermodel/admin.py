d3ab8f0056f396456bb4aadaa8f563b5e4723876
Stu Perry <stewart.c.perry@gmail.com>
2017-11-13 18:46:49 +0000

    Fix line-too-long

--- modules/wsrc/site/usermodel/admin.py
+++ modules/wsrc/site/usermodel/admin.py
@@ -129,7 +129,8 @@ class SubscriptionAdmin(admin.ModelAdmin):
     linked_membership_type.admin_order_field = "membership_type"
 
     def total_payments(self, obj):
-        return "<span style='width:100%; display:inline-block; text-align:right;'>{0:.2f}</span>".format(obj.get_total_payments())
+        return "<span style='width:100%; display:inline-block; text-align:right;'>{0:.2f}</span>"\
+            .format(obj.get_total_payments())
     total_payments.allow_tags = True
     total_payments.short_description = u"Total (\xa3)"
 
