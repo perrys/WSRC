cae043f00744ec475d167d0724a359456ade32db
Stu Perry <stewart.c.perry@gmail.com>
2017-11-17 17:59:29 +0000

    Remove cardnumber from Player form

--- modules/wsrc/site/usermodel/forms.py
+++ modules/wsrc/site/usermodel/forms.py
@@ -107,13 +107,13 @@ class SettingsInfoForm(ModelForm):
         super(SettingsInfoForm, self).__init__(*args, **kwargs)
         instance = getattr(self, 'instance', None)
         if instance and instance.pk:
-            for field in ["cardnumber", "squashlevels_id", "booking_system_id"]:
+            for field in ["squashlevels_id", "booking_system_id"]:
                 self.fields[field].widget.attrs['readonly'] = "readonly"
                 self.fields[field].widget.attrs['disabled'] = "disabled"
             self.fields['membership_type'].widget.attrs['disabled'] = "disabled"
     
     class Meta:
         model = Player
-        fields = ["membership_type",  "booking_system_id", "cardnumber",  "squashlevels_id"]
+        fields = ["membership_type",  "booking_system_id", "squashlevels_id"]
         exclude = ('user',)
 
