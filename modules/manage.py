897dbc15e4de8f12f5974bcf34c7243c754dcbfd
Stu Perry <stewart.c.perry@gmail.com>
2017-11-22 00:35:03 +0000

    Set basic logging config

--- modules/manage.py
+++ modules/manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+import logging
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)-10s [%(levelname)s] %(message)s',datefmt="%Y-%m-%d %H:%M:%S")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wsrc.site.settings.settings")
 
     from django.core.management import execute_from_command_line
