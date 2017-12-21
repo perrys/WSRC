5389317e0226c34c4d3684bef867d99c3d54a4a9
Stu Perry <stewart.c.perry@gmail.com>
2017-12-21 08:27:58 +0000

    Construct a depth-2 tree of navigation nodes and links (leaves)

--- modules/wsrc/site/navigation.py
+++ modules/wsrc/site/navigation.py
@@ -15,12 +15,32 @@
 
 "Middleware to insert navigation data into template context"
 
-from wsrc.site.models import NavigationLink
+from django.core.urlresolvers import reverse
+
+from wsrc.site.models import NavigationLink, NavigationNode
+from collections import namedtuple
+
+node_t = namedtuple("Node", ["pk", "is_restricted", "name", "children", "is_expanded"])
 
 class NavigationMiddleWare:
     def process_template_response(self, request, response):
+        nodes = NavigationNode.objects.filter(ordering__gt=0)
         links = NavigationLink.objects.filter(ordering__gt=0)
         if not request.user.is_authenticated():
+            nodes = nodes.exclude(is_restricted=True)
             links = links.exclude(is_restricted=True)
-        response.context_data["navlinks"] = links
+        nodes = [node_t(node.pk, node.is_restricted, node.name, [], False) for node in nodes]
+        nodes_map = dict([(n.pk, n) for n in nodes])
+        for link in links:
+            if link.parent_id is not None:
+                parent = nodes_map[link.parent_id]
+                parent.children.append(link)
+                del nodes_map[link.pk]
+                url = reverse(link.url) if link.is_reverse_url else link.url
+                if request.path.startswith(url):
+                    nodes_map[link.parent_id] = parent._replace(is_expanded=True)
+            else:
+                nodes_map[link.pk] = link
+        nodes = [nodes_map[node.pk] for node in nodes if node.pk in nodes_map]
+        response.context_data["navlinks"] = nodes
         return response
