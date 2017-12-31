0264e55de49bf501b97af2264e01a7a5067ee282
Stu Perry <stewart.c.perry@gmail.com>
2017-12-31 13:02:24 +0000

    Add is_age_sensitive accessor to Subscription model

--- modules/wsrc/site/usermodel/models.py
+++ modules/wsrc/site/usermodel/models.py
@@ -264,6 +264,8 @@ class Subscription(models.Model):
                 return True
         return False
 
+    def is_age_sensitive(self):
+        return self.subscription_type.max_age_years is not None
 
     def __unicode__(self):
         return u"{0} ({1}) \u2013 {2}".format(self.player.get_ordered_name(), self.season, self.subscription_type.name)
