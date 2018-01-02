0d784147957bcdc9c3dd8f5bdba8c17497cc59c1
Stu Perry <stewart.c.perry@gmail.com>
2018-01-02 23:01:49 +0000

    Allow encoding of get_ordered_name; add short descr for subscription

--- modules/wsrc/site/usermodel/models.py
+++ modules/wsrc/site/usermodel/models.py
@@ -61,11 +61,13 @@ class Player(models.Model):
         return None
     get_current_subscription.short_description = 'Subscription'
 
-    def get_ordered_name(self):
+    def get_ordered_name(self, encoding=None):
         """
         Returns the last_name plus the first_name, with a comma in between.
         """
         full_name = '%s, %s' % (self.user.last_name, self.user.first_name)
+        if encoding is not None:
+            full_name = full_name.encode(encoding)
         return full_name.strip()
 
     def get_cardnumbers(self):
@@ -264,6 +266,8 @@ class Subscription(models.Model):
                 return True
         return False
 
+    def to_short_string(self):
+        return u"{0} ({1})".format(self.subscription_type.name, self.season)
 
     def __unicode__(self):
         return u"{0} ({1}) \u2013 {2}".format(self.player.get_ordered_name(), self.season, self.subscription_type.name)
