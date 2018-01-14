1d6822d57e0fc56f59425c154cdc762723549e7a
Stu Perry <stewart.c.perry@gmail.com>
2018-01-14 09:05:06 +0000

    Remove legacy JQM and bxslider files

--- resources/js/pre_jqm.js
+++
@@ -1,8 +0,0 @@
-$(document).on( "mobileinit", function() {
-  $(document).on( "pagecontainershow", function( event, ui ) {
-    if (window.WSRC === undefined)
-      window.WSRC_deferedPageContainerShow = [event, ui]
-    else
-      WSRC.onPageContainerShow(event, ui)
-  });
-});
