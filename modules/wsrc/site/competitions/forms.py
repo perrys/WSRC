dd4eb005236c56b7fd7e14df15c8bcd7b913763e
Stu Perry <stewart.c.perry@gmail.com>
2018-02-04 00:36:01 +0000

    Allow match choice on scores form; fix tabindex ordering

--- modules/wsrc/site/competitions/forms.py
+++ modules/wsrc/site/competitions/forms.py
@@ -19,6 +19,7 @@
 from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
 from django import forms
 from django.core.urlresolvers import reverse
+from django.db.models import Q
 
 from wsrc.utils.form_utils import make_readonly_widget, LabeledSelect, CachingModelChoiceField, add_formfield_attrs
 from wsrc.site.competitions.models import Match, Entrant
@@ -29,10 +30,15 @@ class EntrantChoiceField(CachingModelChoiceField):
     def label_from_instance(self, entrant):
         return entrant.get_players_as_string()
 
+class MatchChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, match):
+        return match.get_teams_display()
+
 class MatchScoresForm(forms.ModelForm):
     entrant_queryset = Entrant.objects.select_related("player1__user", "player2__user")
     team1 = EntrantChoiceField(queryset=entrant_queryset, label="Opponent 1")
     team2 = EntrantChoiceField(queryset=entrant_queryset, label="Opponent 2")
+    match = MatchChoiceField(queryset=Match.objects.none(), label="Match")
 
     class Meta:
         model = Match
@@ -40,32 +46,62 @@ class MatchScoresForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.comp_id = kwargs.pop('comp_id', None)
-        if self.comp_id is not None:
-            self.comp_id = int(self.comp_id)
+        if self.comp_id is None:
+            raise Exception("no competition ID supplied for match scores form")
+        self.comp_id = int(self.comp_id)
+        
         mode = kwargs.pop('mode', None)
         with_teams = kwargs.pop('with_teams', False)
         super(MatchScoresForm, self).__init__(*args, **kwargs)
+        
         self.fields['competition'].widget = forms.HiddenInput()
         self.fields['walkover'].label = "Walkover To"
         walkover_choices = Match.WALKOVER_RESULTS
+
+        tabindex = [1]
+        def set_tab_index(field, resolve=True):
+            if resolve:
+                field = self.fields[field]
+            field.widget.attrs["tabindex"] = tabindex[0]                
+            tabindex[0] += 1
+
+        set_tab_index("team1")
+        set_tab_index("team2")
+        set_tab_index("match")
+
         if "update" == mode:
-            match = kwargs["instance"]
-            walkover_choices = [(1, match.team1.get_players_as_string()),
-                                (2, match.team2.get_players_as_string())]
+            self.fields.pop("match")
             if not with_teams:
                 self.fields.pop('team1')
                 self.fields.pop('team2')
-        elif self.comp_id:
+            match = kwargs["instance"]
+            walkover_choices = [(1, match.team1.get_players_as_string()),
+                                (2, match.team2.get_players_as_string())]
+        elif "choose_and_update" == mode:
+            self.fields.pop('team1')
+            self.fields.pop('team2')
+            match_queryset = Match.objects.select_related("team1__player1__user", "team1__player2__user",\
+                                                          "team2__player1__user", "team2__player2__user")
+            match_queryset = match_queryset.filter(competition_id=self.comp_id)
+            match_queryset = match_queryset.filter(team1__isnull=False, team2__isnull=False)
+            match_queryset = match_queryset.filter(Q(team1_score1__isnull=True) & Q(walkover__isnull=True))
+            self.fields["match"].queryset = match_queryset
+        else:
+            self.fields.pop("match")
             entrant_queryset = self.entrant_queryset.filter(competition_id=self.comp_id)
             self.fields['team1'].queryset = entrant_queryset
             self.fields['team2'].queryset = entrant_queryset
+
         self.fields['walkover'].widget = forms.RadioSelect(choices=[('', '(None)')] + walkover_choices)
-        choices = [(idx, idx) for idx in range(50,-1, -1)] + [("", "")]
-        if False:
-            choices = choices + [(idx, idx) for idx in range(-1,-51, -1)] # prefix this for handicaps
-        for team in (1, 2):
-            for aset in range(1, 6):
-                self.fields["team{team}_score{aset}".format(**locals())].widget = forms.widgets.Select(choices=choices)
+        score_choices = [("", "")] + [(idx, idx) for idx in range(0, 51, 1)]
+        if False: # TODO: choose handicap competitions
+            score_choices = [(idx, idx) for idx in range(-50, 0, 1)] + score_choices# prefix this for handicaps
+
+        for aset in range(1, 6):
+            for team in (1, 2):
+                field = self.fields["team{team}_score{aset}".format(**locals())]
+                field.widget = forms.widgets.Select(choices=score_choices)
+                set_tab_index(field, False)
         add_formfield_attrs(self)
 
     def clean(self):
