8210803aa9dff91fdaa9e5c2903b8c2a1529191f
Stu Perry <stewart.c.perry@gmail.com>
2018-03-04 10:58:07 +0000

    Add sync logic when people unsubscribe from the mailchimp side

--- modules/wsrc/external_sites/mailchimp.py
+++ modules/wsrc/external_sites/mailchimp.py
@@ -62,10 +62,16 @@ class MailChimpSession:
                     data["accountname"], data["role"], data["api_endpoint"])
         self.server_uri = data["api_endpoint"]
         self.list_id = settings.MAILCHIMP_LIST_ID
+        self.list_info = self.api_get("/lists/{id}".format(id=self.list_id))
+
+    @classmethod
+    def is_subscribed(clz, player):
+        pref = player.prefs_receive_email
+        return pref != False # TODO: change this for GDPR May 2018
 
     def sync(self):
         member_map = self.get_users()
-        LOGGER.info("Found: %s unique email addresses with subscribed=yes", len(member_map))
+        LOGGER.info("Found: %s unique email addresses with current subscriptions", len(member_map))
         category_to_interest_map_map = self.sync_interest_groups(member_map)
         return self.sync_users(member_map, category_to_interest_map_map)
 
@@ -103,41 +109,54 @@ class MailChimpSession:
 
     def sync_users(self, member_map, category_to_interest_map_map):
         mailchimp_list = self.api_get("/lists/{id}/members?count=9999".format(id=self.list_id))
-        to_remove = set()
+        to_remove = []
         mailchimp_map = dict()
-        for member in mailchimp_list["members"]:
-            db_id = member["merge_fields"].get("DB_ID")
-            if db_id:
-                mailchimp_map[int(db_id)] = member
+        for mailchimp_record in mailchimp_list["members"]:
+            db_id = mailchimp_record["merge_fields"].get("DB_ID")
+            if db_id is not None:
+                db_id = int(db_id)
+                if db_id > 0: # ignore negative ids, which are acknowleged skipped emails
+                    mailchimp_map[db_id] = mailchimp_record
             else:
-                to_remove.add((member["id"], member["email_address"]))
+                to_remove.append(mailchimp_record)
         if len(to_remove) > 0:
             LOGGER.warning("Found %s mailchimp entries without DB_ID", len(to_remove))
-        member_ids = set(member_map.keys())
+
+        subscribed_ids = set([key for key,val in member_map.items() if self.is_subscribed(val.player)])
         mailchimp_ids = set(mailchimp_map.keys())
 
         errors = []
-        for db_id in mailchimp_ids - member_ids:
+        for db_id in mailchimp_ids - subscribed_ids:
+            # we have an id in mailchimp not in our subscribed list
             mailchimp_record = mailchimp_map[db_id]
-            to_remove.add((mailchimp_record["id"], mailchimp_record["email_address"]))
+            if mailchimp_record.get("status") == "unsubscribed" or mailchimp_record["merge_fields"].get("UNSUB_SYNC", 0) == 1:
+                # person unsubscribed at some point - leave for merge logic
+                continue
+            # simple case - our member's subs lapsed or they removed
+            # their email preference, so we will just take them out of
+            # the list:
+            to_remove.append(mailchimp_record)
+
         LOGGER.info("Have %s email address(es) to remove", len(to_remove))
         errors.extend(self.remove(to_remove))
 
-        to_add = member_ids - mailchimp_ids
+        to_add = subscribed_ids - mailchimp_ids
         LOGGER.info("Have %s email address(es) to add", len(to_add))
         errors.extend(self.add(member_map, to_add, category_to_interest_map_map))
 
-        to_compare = member_ids.intersection(mailchimp_ids)
+        # take the intersection of all DB members and mailchimp records
+        to_compare = set(member_map.keys()).intersection(mailchimp_ids)
         LOGGER.info("Have %s email address(es) to compare", len(to_compare))
-        self.compare(mailchimp_map, member_map, to_compare, category_to_interest_map_map)
+        errors.extend(self.compare(mailchimp_map, member_map, to_compare, to_remove, category_to_interest_map_map))
+
         return errors
 
-    def remove(self, ids):
+    def remove(self, records):
         errors = []
-        for (hash_id, email) in ids:
-            endpoint = "/lists/{id}/members/{hash_id}".format(id=self.list_id, hash_id=hash_id)
+        for mailchimp_record in records:
+            endpoint = "/lists/{id}/members/{hash_id}".format(id=self.list_id, hash_id=mailchimp_record["id"])
             try:
-                LOGGER.info("Removing %s", email)
+                LOGGER.info("Removing %s", mailchimp_record["email_address"])
                 self.api_delete(endpoint)
             except APIException, exc:
                 errors.append(exc)
@@ -154,7 +173,8 @@ class MailChimpSession:
                 "merge_fields": {
                     "FNAME": email_user.player.user.first_name,
                     "LNAME": email_user.player.user.last_name,
-                    "DB_ID": email_user.player.id
+                    "DB_ID": email_user.player.id,
+                    "UNSUB_SYNC": 0
                 }
             }
             interests = {}
@@ -165,16 +185,52 @@ class MailChimpSession:
                     interests[interest_id] = True
             data["interests"] = interests
             try:
-                LOGGER.info("Adding %s %s: %s", email_user.player.user.first_name, email_user.player.user.last_name, email_user.player.user.email)
+                LOGGER.info("Adding %s %s: %s",
+                            email_user.player.user.first_name, email_user.player.user.last_name, email_user.player.user.email)
                 self.api_add(endpoint, data)
             except APIException, exc:
                 errors.append(exc)
         return errors
 
-    def compare(self, mailchimp_map, member_map, to_compare, category_to_interest_map_map):
+    def compare(self, mailchimp_map, member_map, to_compare, already_removed_records, category_to_interest_map_map):
+        errors = []
         for db_id in to_compare:
-            member = member_map[db_id]
             mailchimp_record = mailchimp_map[db_id]
+            if mailchimp_record in already_removed_records:
+                continue
+
+            member = member_map[db_id]
+            endpoint = "/lists/{id}/members/{hash_id}".format(id=self.list_id, hash_id=mailchimp_record["id"])
+
+            if mailchimp_record["status"] == "unsubscribed":
+                # this person unsubscribed themselves at some point, may need to sync back to our DB
+                if mailchimp_record["merge_fields"].get("UNSUB_SYNC", 0) != 1:
+                    LOGGER.info("Unsubscribe request from %s, reason: \"%s\"", member.player.user.get_full_name(),
+                                mailchimp_record.get("unsubscribe_reason"))
+                    member.player.prefs_receive_email = False
+                    member.player.save()
+                    updates = {
+                        "merge_fields": {"UNSUB_SYNC": 1}
+                    }
+                    LOGGER.info("Marking unsubscribe synced for %s", member.player.user.email)
+                    self.api_update(endpoint, updates)
+                elif member.player.prefs_receive_email:
+                    # tricky situation - they resubscribed themselves
+                    # on our database, but mailchimp will not let us
+                    # resubscribe them programatically. They will need
+                    # to fill in the list signup form
+                    errors.append(Exception("Cannot resubscribe \"{0} <{1}>\", they need to sign up via:\n\n{2}"\
+                                            .format(member.player.user.get_full_name(), member.player.user.email,
+                                                    self.list_info["subscribe_url_long"])))
+                continue # no further processing on unsubscribed records
+
+            assert mailchimp_record["status"] == "subscribed"
+            if member.player.prefs_receive_email is False:
+                # this person has resubscribed to mailchimp - reflect that back to our DB
+                LOGGER.info("Subscribe request from %s, syncing to database", member.player.user.get_full_name())
+                member.player.prefs_receive_email = True
+                member.player.save()
+
             expected_interests = {}
             for cat_name, interest_map in category_to_interest_map_map.iteritems():
                 user_interests = member.attributes.get(cat_name, [])
@@ -183,10 +239,17 @@ class MailChimpSession:
             expected_merge_fields = {
                 u"LNAME": member.player.user.last_name,
                 u"FNAME": member.player.user.first_name,
-                u"DB_ID": int(member.player.id)
+                u"DB_ID": int(member.player.id),
+                u"UNSUB_SYNC": 0
             }
+
+            def different_interests(lhs, rhs):
+                ticked_fields = lambda(x): [key for key, val in x.iteritems() if val]
+                lhs, rhs = [set(ticked_fields(x)) for x in (lhs, rhs)]
+                return lhs != rhs
+
             if mailchimp_record["merge_fields"] != expected_merge_fields or \
-               mailchimp_record["interests"] != expected_interests or \
+               different_interests(mailchimp_record["interests"], expected_interests) or \
                mailchimp_record["email_address"] != member.player.user.email:
                 updates = {
                     "merge_fields": expected_merge_fields,
@@ -194,28 +257,52 @@ class MailChimpSession:
                     "email_address": member.player.user.email
                 }
                 LOGGER.info("Updating %s", member.player.user.email)
-                endpoint = "/lists/{id}/members/{hash_id}".format(id=self.list_id, hash_id=mailchimp_record["id"])
                 self.api_update(endpoint, updates)
-
+        return errors
     def get_users(self):
         subs = Subscription.objects.filter(season__has_ended=False, player__user__email__isnull=False)\
-                                   .exclude(player__user__email="")\
-                                   .exclude(player__prefs_receive_email=False)\
                                    .select_related("player__user", "subscription_type")\
-                                   .prefetch_related("player__user__groups")\
-                                   .order_by("subscription_type")
-        members = dict()
-        emails = set()
+                                   .prefetch_related("player__user__groups")
+
+        # mailchimp only allows one entry in the list per email. When
+        # we have multiple members in our DB with the same address
+        # (e.g. juniors with parents' email) , we just pick the most
+        # "senior" one and sync them to the list. If members want
+        # separate emails for juniors they will need to use individual
+        # email addresses.
+        emails = dict()
         for sub in subs:
-            email = sub.player.user.email.lower()
-            if email in emails:
-                continue # cannot have two mailchimp entries for the same email, e.g. parent and child
-            emails.add(email)
+            email = sub.player.user.email.lower().strip()
+            if len(email) == 0:
+                continue
+            subs_with_email = emails[email] = emails.get(email, [])
+            subs_with_email.append(sub)
+
+        def seniority_cmp(lhs, rhs):
+            # first compare by age
+            lhsdob, rhsdob = [x.player.date_of_birth for x in (lhs, rhs)]
+            lhsdob, rhsdob = [x.toordinal() if x is not None else 0 for x in (lhsdob, rhsdob)]
+            result = lhsdob - rhsdob
+            if result == 0: # fall back to membership id
+                result = lhs.player.id - rhs.player.id
+            return int(result)
+
+        subs = []
+        for subs_with_email in emails.itervalues():
+            if len(subs_with_email) > 1:
+                subs_with_email.sort(seniority_cmp)
+            subs.append(subs_with_email[0]) # earliest DoB or earliest db id
+
+        # now we have a unique set of emails, add other attributes:
+        members = dict()
+        for sub in subs:
+            # subs type and entitlement groups:
             members[sub.player.id] = user = EmailUser(sub.player)
             user.add_attribute("Subscription", sub.subscription_type.name)
             for group in sub.player.user.groups.all():
                 user.add_attribute("Roles", group.name)
 
+        # competitions entered:
         def attribute(players, comp_name):
             for player in players:
                 user = members.get(player.id)
